71e4902f7e356fb924ddc31c065c8ca9e4d8346b
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-11-25 21:48:30 +0530

    Data types revesion

--- script.js
+++ script.js
@@ -35,3 +35,33 @@ console.log(a);
 var a=12;
 //this will be divided as var a=undefined;//this is goes to top 
 a=12; // thisis another line
+
+
+//Datatypes in js
+/*
+primitives->aise saare values copy karne par ek real copy milte hai example:string,number,boolen,null,undefined,symbol,bigint
+reference->aise saare copy karne se reference milti hai Example: arrays,objects,functions 
+''-single quotes
+""-double quotes
+``-backticks
+12 -integer 
+12.3 -number 
+
+symbol ->unique identifier 
+future mein hum koi libraries use karenge ab is mein un libraries mein kai baar kucch fields hoti hai jisnse similar hum bhi banaa dete hai aur galti se hamari banaai hui fields us library ki original fields ko change kar deta hai
+let u1=Symbol('id');
+let u2=Symbol("id");
+console.log(u1===u2); //false because symbol is unique
+let a= 9000097475678482n;
+a+2n;
+*/
+
+/*Dynamic typing -> js mein static typing nahi hai and yaha par hai dynamic typing hai aap data ko chage kar sakte ho kyunki yaha par dynamic datatypes hai
+typeof
+
+type coercion->concept jis mein aapka ek type automatically convert hojaayega
+
+"5"+1=51  here + does two things concat and add it has one string so it concats 
+"5"-1=4   here - has one operation only subtraction so it converts str to int
+0 false '' null undefined NaN document.all ->falsy values
+*/
