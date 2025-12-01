851e48055eff86aa3a86d84c5adcc1ac4a838301
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-12-01 21:42:24 +0530

    DOM Manipulation

--- script2.js
+++ script2.js
@@ -6,3 +6,14 @@ let ab=document.querySelector(".abcd");*/
 let h1=document.querySelector("h1");
 h1.textContent="Hello Chinmayi CEO of Google";
 h1.innerHTML="<i>Hello Chinmayi CEO Of google</i>"
+let a= document.querySelector("a");
+a.href="https://omnicognitoo.blogspot.com";
+a.setAttribute("href","https://omnicognitoo.blogspot.com");
+let img = document.querySelector("img");
+img.setAttribute("src","https://plus.unsplash.com/premium_photo-1764435536930-c93558fa72c6?q=80&w=1223&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+console.log(a.getAttribute("href"));
+a.removeAttribute("href");
+
+let h1=document.createElement("h1");
+h1.textContent="This is created by JS";
+document.body.appendChild(h1);
