6dbe31497ba3228179badfa0988f9f1485ab0e04
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-12-01 21:15:13 +0530

    Dom manipualtion

--- script2.js
+++ script2.js
@@ -1,2 +1,8 @@
-let abcd = document.getElementsId("abcd");
+/*let abcd = document.getElementsId("abcd");
 console.log(abcd);
+let abc= document.getElementsByClassName("abcd");
+console.log(abc);
+let ab=document.querySelector(".abcd");*/
+let h1=document.querySelector("h1");
+h1.textContent="Hello Chinmayi CEO of Google";
+h1.innerHTML="<i>Hello Chinmayi CEO Of google</i>"
