fa353762bc94ee00c1115e1fdfe76f592988871e
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-12-02 07:50:25 +0530

    Dom manipualtion practice

--- script2.js
+++ script2.js
@@ -3,7 +3,7 @@ console.log(abcd);
 let abc= document.getElementsByClassName("abcd");
 console.log(abc);
 let ab=document.querySelector(".abcd");*/
-let h1=document.querySelector("h1");
+/*let h1=document.querySelector("h1");
 h1.textContent="Hello Chinmayi CEO of Google";
 h1.innerHTML="<i>Hello Chinmayi CEO Of google</i>"
 let a= document.querySelector("a");
@@ -12,8 +12,31 @@ a.setAttribute("href","https://omnicognitoo.blogspot.com");
 let img = document.querySelector("img");
 img.setAttribute("src","https://plus.unsplash.com/premium_photo-1764435536930-c93558fa72c6?q=80&w=1223&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
 console.log(a.getAttribute("href"));
-a.removeAttribute("href");
+a.removeAttribute("href");*/
 
-let h1=document.createElement("h1");
-h1.textContent="This is created by JS";
-document.body.appendChild(h1);
+// let h1=document.createElement("h1");
+// h1.textContent="This is created by JS";
+// document.body.appendChild(h1);
+// h1.remove();
+let h1=document.querySelector("h1");
+h1.textContent="hai my name is chinmayi";
+document.querySelector("div").prepend(h1);
+h1.style.color="red";
+h1.classList.add("abcd");
+h1.classList.remove("abcd");
+h1.classList.toggle("abcd");
+
+document.querySelector("apple");
+document.querySelector("#banana");//id
+let l=document.querySelectorAll(".buy-now");
+console.log(l);
+let lis=document.querySelectorAll("li");
+lis.forEach(function(val){
+    console.log(val.textContent);
+})
+for(let i=0;i<lis.length;i++){
+    console.log(lis[i].textContent);
+}
+
+//textContent and innerText these are almost same ->in this if we write <i> this will print as it is it is not converted to convert it we have to do in innnerHTML
+document.querySelector("p").innerHTML="<b>hey this is part-2 java tutorial</b>"
