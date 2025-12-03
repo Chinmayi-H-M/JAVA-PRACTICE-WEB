3130ec06c55f5d054e5ce1495383bdfecb36b964
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-12-03 18:45:34 +0530

    Event practice

--- events.js
+++ events.js
@@ -8,10 +8,24 @@ h1.addEventListener("click",function(){
 function dblclick(){
     h1.style.color="green";
 }
-h1.addEventListener("dblclick",dblclick);
+/*h1.addEventListener("dblclick",dblclick);
 h1.removeEventListener("dblclick",dblclick);
 let input = document.querySelector("input");
 input.addEventListener("input",function(){
     console.log("typed");
+});*/
+let inp = document.querySelector("input");
+inp.addEventListener("input",function(dets){
+    if(dets.data!==null){
+        console.log(dets.data);
+    }
 });
+//tab chaltha hai jab aapka koi input select ya textarea mein koi changes hojaaye
+let sel=document.querySelector("select");
+let device=document.querySelector("#device");
+sel.addEventListener("change",function(dets){
+    console.log(dets.target.value);
+    device.textContent="Device Selected";
+    device.textContent=`${dets.target.value} device selected`;
+})
 
