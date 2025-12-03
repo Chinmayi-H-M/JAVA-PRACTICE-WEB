f0abfe52b5876b8c38a1c8388222f8f798b4e07d
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-12-03 18:01:16 +0530

    Events

---
+++ events.js
@@ -0,0 +1,7 @@
+//When we do anyting in the browser that raises the events
+//event matlab hota hai koi action hua
+//event listener ka matlab hai aapne koi action ka reaction diya
+let h1=document.querySelector("h1");
+h1.addEventListener("click",function(){
+    h1.style.color="red";
+})
--- script2.js
+++ script2.js
@@ -80,3 +80,4 @@ p.forEach(element => {
 
 
 
+
