59fabd649357b24329d895af5f22100144682285
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-12-04 20:58:18 +0530

    Profile adding dynamically

--- events.js
+++ events.js
@@ -28,7 +28,8 @@ sel.addEventListener("change",function(dets){
     device.textContent="Device Selected";
     device.textContent=`${dets.target.value} device selected`;
 })*/
-let h1=document.querySelector("h1");
+/*let h1=document.querySelector("h1");
+
 window.addEventListener("keydown",function(dets){
     if(dets.key===" "){
         h1.textContent="spc";
@@ -49,5 +50,42 @@ input.addEventListener("change",function(dets){
     if (file){
        btn.textContent=file.name;
     }
-})
+})*/
+let form = document.querySelector("form");
+let inputs = document.querySelectorAll("input");
+let main= document.querySelector("#main");
+form.addEventListener("submit",function(dets){
+    dets.preventDefault();
+    // console.log(inputs[0].value,
+    //     inputs[1].value,
+    //     inputs[2].value,
+    //     inputs[3].value,
+    let card = document.createElement("div");
+    card.classList.add("card");
+    let profile =document.createElement("div");
+    profile.classList.add("profile");
+    let img = document.createElement("img");
+    img.setAttribute("src",inputs[0].value);
+    let h3= document.createElement("h3");
+    h3.textContent=inputs[1].value;
+    let h5= document.createElement("h5");
+    h5.textContent=inputs[2].value;
+    let p= document.createElement("p");
+    p.textContent=inputs[3].value;
+    profile.appendChild(img);
+
+    card.appendChild(profile);
+    card.appendChild(h3);
+    card.appendChild(h5);
+    card.appendChild(p);
+
+    main.appendChild(card);
+    inputs.forEach(function(inp){
+        if(inp.type!=="submit"){
+            inp.value="";
+        }
+
+    }
+    );
+});
 
