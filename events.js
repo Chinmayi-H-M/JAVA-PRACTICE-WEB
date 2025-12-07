726de78c168a414965b1ea46447f36f317e10af2
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-12-07 19:02:36 +0530

    Mouseover and mouse out event handling

--- events.js
+++ events.js
@@ -128,3 +128,11 @@ form.addEventListener("submit",function(dets){
     );
 });
 
+let abcd = document.querySelector("#abcd");
+abcd.addEventListener("mouseover", function(){
+    abcd.style.backgroundColor = "yellow";
+});
+abcd.addEventListener("mouseout", function(){
+    abcd.style.backgroundColor = "red";
+})
+
