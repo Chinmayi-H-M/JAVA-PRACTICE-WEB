d6d316ce1fd433d26e78860b0e99fefed47c3399
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-12-08 21:03:37 +0530

    Event bubbling in li

--- events.js
+++ events.js
@@ -148,5 +148,10 @@ window.addEventListener("mousemove",function(dets){
 document.querySelector("#nav").addEventListener("click",function(){
     alert("Clicked");
 });
+let ul= document.querySelector("ul");
+ul.addEventListener("click",function(dets){
+    //dets.target.style.textDecoration="line-through";
+    dets.target.classList.toggle("lt");
+});
 
 
