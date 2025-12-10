062f49fed257a035ea85399899e27fa3d211fb78
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-12-10 20:33:31 +0530

    Storages

---
+++ storage/script.js
@@ -0,0 +1,6 @@
+//localstorgae
+//store karne ke liye - setItem
+//data fetch karne ke liye - getItem
+//remove item karne ke liye - removeItem
+//LOcal storage cannot store objects and other data types it only stores the strings 
+//so we use JSON.stringify and for converting back to it we use JSON.parse
