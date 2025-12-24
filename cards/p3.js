47e596caf58c41bfa2a16d287367de81720237ca
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-12-24 11:18:05 +0530

    Added form functionality

--- cards/p3.js
+++ cards/p3.js
@@ -3,3 +3,10 @@
 //buttons ko handle karna hai
 //filters ko handle karna hai
 
+let addNoteBtn = document.querySelector("#add-note-btn");
+let formContainer = document.querySelector(".form-container")
+
+addNoteBtn.addEventListener("click",function(){
+
+    formContainer.style.display = "initial";
+})
