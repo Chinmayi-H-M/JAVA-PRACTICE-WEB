05ed3b46372d3a11b97c26b04eb230a68ad0a018
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-12-24 15:04:43 +0530

    Display features

--- cards/p3.js
+++ cards/p3.js
@@ -4,9 +4,17 @@
 //filters ko handle karna hai
 
 let addNoteBtn = document.querySelector("#add-note-btn");
-let formContainer = document.querySelector(".form-container")
+let formContainer = document.querySelector(".form-container");
+let noteWrapper = document.querySelector(".note-wrapper");
+let closeBtn = document.querySelector("#close-btn");
 
 addNoteBtn.addEventListener("click",function(){
 
     formContainer.style.display = "initial";
+    noteWrapper.style.display = "none";
+})
+
+closeBtn.addEventListener("click",function(){
+    formContainer.style.display = "none";
+    noteWrapper.style.display = "block";
 })
