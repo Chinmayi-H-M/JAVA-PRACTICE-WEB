fa70296c64dc67eaabc184f7b14683ee532e1e1e
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-12-24 19:58:41 +0530

    form validation error handling

--- cards/p3.js
+++ cards/p3.js
@@ -16,10 +16,25 @@ const homeTownInput = form.querySelector('input[placeholder="Enter home town"]')
 const purposeInput = form.querySelector('input[placeholder="e.g. Quick appointment note"]');
 const categoryInputs = form.querySelectorAll('input[name="category"]');
 let selected = false;
+function saveToLocalStrorage(obj){
+    if(localStorage.getItem("tasks")===null){
+        let oldTasks=[];
+        oldTasks.push(obj);
+        ol=JSON.stringify(oldTasks);
+        localStorage.setItem("tasks",ol);
+    }
+    else{
+        let oldTasks= localStorage.getItem("tasks");
+        oldTasks=JSON.parse(oldTasks);
+        oldTasks.push(obj);
+        ol=JSON.stringify(oldTasks);
+        localStorage.setItem("tasks",ol);
+    }
+
+}
 
 addNoteBtn.addEventListener("click", function () {
-
-    formContainer.style.display = "initial";
+formContainer.style.display = "initial";
     noteWrapper.style.display = "none";
 })
 
@@ -28,12 +43,13 @@ closeBtn.addEventListener("click", function () {
     noteWrapper.style.display = "block";
 })
 
-form.addEventListener("submit", function makeValidation(Event) {
-    Event.preventDefault();
-    if (imageUrlInput.value.trim() === "" && fullNameInput.value.trim() === "" && homeTownInput.value.trim() === "" && purposeInput.value.trim() === "") {
+form.addEventListener("submit", function makeValidation(e) {
+    e.preventDefault();
+    if (imageUrlInput.value.trim() === "" || fullNameInput.value.trim() === "" || homeTownInput.value.trim() === "" || purposeInput.value.trim() === "") {
         alert("Please fill in all the fields appropriate information.");
         return;
     }
+    let selected = "";
     categoryInputs.forEach(function (cats) {
         if (cats.checked) {
             selected = cats.value;
@@ -43,5 +59,15 @@ form.addEventListener("submit", function makeValidation(Event) {
         alert("Please select a category.");
         return;
     }
+
+    saveToLocalStrorage({
+    imageUrl: imageUrlInput.value,
+    fullName: fullNameInput.value,
+    purpose: purposeInput.value,
+    homeTown: homeTownInput.value,
+    category: selected
+});
+
+    form.reset();
 
 })
