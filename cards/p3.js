71d686de1a0cd60d0fe73667756710613b210542
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-12-25 00:08:04 +0530

    Js

--- cards/p3.js
+++ cards/p3.js
@@ -2,7 +2,7 @@
 //localstorage se hi cards ko show karna hai
 //buttons ko handle karna hai
 //filters ko handle karna hai
-const tasks = [];
+document.addEventListener("DOMContentLoaded",function(){
 
 let addNoteBtn = document.querySelector("#add-note-btn");
 let formContainer = document.querySelector(".form-container");
@@ -15,23 +15,23 @@ const fullNameInput = form.querySelector('input[placeholder="Enter full name"]')
 const homeTownInput = form.querySelector('input[placeholder="Enter home town"]');
 const purposeInput = form.querySelector('input[placeholder="e.g. Quick appointment note"]');
 const categoryInputs = form.querySelectorAll('input[name="category"]');
-let selected = false;
-function saveToLocalStrorage(obj){
+
+function saveToLocalStorage(obj){
     if(localStorage.getItem("tasks")===null){
         let oldTasks=[];
         oldTasks.push(obj);
-        ol=JSON.stringify(oldTasks);
+        let ol=JSON.stringify(oldTasks);
         localStorage.setItem("tasks",ol);
     }
     else{
         let oldTasks= localStorage.getItem("tasks");
         oldTasks=JSON.parse(oldTasks);
         oldTasks.push(obj);
-        ol=JSON.stringify(oldTasks);
+        let ol=JSON.stringify(oldTasks);
         localStorage.setItem("tasks",ol);
     }
-
 }
+
 
 addNoteBtn.addEventListener("click", function () {
 formContainer.style.display = "initial";
@@ -43,7 +43,7 @@ closeBtn.addEventListener("click", function () {
     noteWrapper.style.display = "block";
 })
 
-form.addEventListener("submit", function makeValidation(e) {
+form.addEventListener("submit", function (e) {
     e.preventDefault();
     if (imageUrlInput.value.trim() === "" || fullNameInput.value.trim() === "" || homeTownInput.value.trim() === "" || purposeInput.value.trim() === "") {
         alert("Please fill in all the fields appropriate information.");
@@ -60,7 +60,7 @@ form.addEventListener("submit", function makeValidation(e) {
         return;
     }
 
-    saveToLocalStrorage({
+    saveToLocalStorage({
     imageUrl: imageUrlInput.value,
     fullName: fullNameInput.value,
     purpose: purposeInput.value,
@@ -71,3 +71,4 @@ form.addEventListener("submit", function makeValidation(e) {
     form.reset();
 
 })
+});
