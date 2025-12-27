2a3d0ea9a4c8d5b40a24ea7ee644392e620df73b
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-12-27 17:31:52 +0530

    Different types of scopes

--- Advanced_js/scope.js
+++ Advanced_js/scope.js
@@ -1,2 +1,5 @@
 //scope- functional scope and global scope
 
+//functional scope - function ke andhar hi use kar sakthe hai
+//global scope - poore code mein kahi bhi use kar sakthe hai
+//block scope - {} curly braces ke andhar hi use karte hai
