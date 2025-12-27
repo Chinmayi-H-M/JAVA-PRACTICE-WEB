f4eb662bc49c46b8a10b2c576a2fc8db5d4d3b3d
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-12-27 17:54:14 +0530

    Lexical scoping

--- Advanced_js/scope.js
+++ Advanced_js/scope.js
@@ -3,3 +3,16 @@
 //functional scope - function ke andhar hi use kar sakthe hai
 //global scope - poore code mein kahi bhi use kar sakthe hai
 //block scope - {} curly braces ke andhar hi use karte hai
+
+//execution context- jo bhi code run ho raha hai uske liye jo data available hai usko execution context kehte hai
+//memory phase and excution phase
+
+
+//Js -lexical scoping - It means that the scope or what can be accessed depends on determined by its physical placement in the code
+function abcd(){
+    var a= 10;//lexical scope
+    function defg(){
+        console.log(a);
+    }
+}
+//dynamic scoping-
