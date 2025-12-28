332767ebb80509e39aa1703c99b73c14dd4f04c0
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-12-28 19:10:46 +0530

    Creating toaster

--- Advanced_js/scope.js
+++ Advanced_js/scope.js
@@ -76,7 +76,15 @@ fnc1();
 
 
 function createToaster(config){
-    return function(){
-        
+    return function(notification){
+        let div =document.createElement("div")
     }
 }
+
+let toaster = createToaster({
+    positionX:"right",
+    positionY:"top",
+    theme:"dark",
+    duration:3,
+})
+toaster("This is a dummy notification");
