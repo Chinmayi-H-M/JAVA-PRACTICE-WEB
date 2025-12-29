fda84a281c615a304f80fb6122700a16e59854c5
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-12-29 19:53:56 +0530

    Toasters

--- Toasters/toasters.js
+++ Toasters/toasters.js
@@ -5,6 +5,10 @@ function createToaster(config){
         div.className = "inline-block bg-gray-800 text-white px-5 py-3 rounded shadow-lg pointer-events-none transition-opacity duration-300";
 
         document.querySelector(".parent").appendChild(div);
+        if(config.positionX==="right"|| config.positionY==="bottom"){
+            document.querySelector(".parent").className="fixed";
+            document.querySelector(".parent").className+=` ${config.positionX ==="right" ? "right-5" : "left-5"} ${config.positionY ==="bottom" ? "bottom-5":"top-5"};`;
+        }
         setTimeout(()=>{
             document.querySelector(".parent").removeChild(div);
         }, config.duration);
@@ -21,4 +25,6 @@ toaster("Download done!!");
 setTimeout(() => {
     toaster("Chinmai accepted the request");
 }, 2000);
+
+
 
