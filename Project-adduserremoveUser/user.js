7acc7c7bc7d884c43feae94b1459d7e8507c0a2c
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-12-30 16:55:06 +0530

    Usersubmitting details projects

--- Project-adduserremoveUser/user.js
+++ Project-adduserremoveUser/user.js
@@ -1,21 +1,29 @@
 let form = document.querySelector("form");
- const userManager ={
+let username = document.querySelector("#username");
+let role = document.querySelector("#role");
+let bio = document.querySelector("#bio");
+let photo = document.querySelector("#photo");
+const userManager ={
     users : [],
     init: function(){
         form.addEventListener("submit",this.submitForm.bind(this));
     },
     submitForm: function(e){
             e.preventDefault();
-            this.users.push({
+            this.addUser();
+            
+    },
+    addUser: function(){
+        this.users.push({
                 username:username.value,
                 role:role.value,
                 bio:bio.value,
                 photo:photo.value,
             });
-
             form.reset();
+
     },
-    addUser: function(){},
     removeUser: function() {},
  };
  userManager.init();
+ console.log(userManager.users);
