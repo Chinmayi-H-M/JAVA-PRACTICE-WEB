ca59baa0a8065193f899f7f807c4bbc677e28d1d
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-12-30 19:29:20 +0530

    OOPS IN JAVASCRIPT

--- oops/oop.js
+++ oops/oop.js
@@ -2,13 +2,14 @@ function CreatePencil(name,price,color, company){
     this.name = name;
     this.price = price;
     this.color = color;
-    this.company = company;
-    this.write = function(){
+    //this.company = company;
+    
+}
+CreatePencil.prototype.write = function(text){
         let h1 = document.createElement("h1");
-        h1.textContent = `The pencil ${this.name} is writing`;
+        h1.textContent = text;
         document.body.appendChild(h1);
     }
-}
 
 let pencil1=new CreatePencil("Nataraj",10,"Yellow","Nataraj Co.");
 let pencil2=new CreatePencil("Apsara",5,"Black and white","Apsara co.");
