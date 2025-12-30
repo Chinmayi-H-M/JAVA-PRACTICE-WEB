a4089bd0d08de44573b13ab72c578e57c96a0e56
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-12-30 17:14:57 +0530

    Usersubmitting details projects

--- Project-adduserremoveUser/user.js
+++ Project-adduserremoveUser/user.js
@@ -3,27 +3,63 @@ let username = document.querySelector("#username");
 let role = document.querySelector("#role");
 let bio = document.querySelector("#bio");
 let photo = document.querySelector("#photo");
-const userManager ={
-    users : [],
-    init: function(){
-        form.addEventListener("submit",this.submitForm.bind(this));
+const userManager = {
+    users: [],
+    init: function () {
+        form.addEventListener("submit", this.submitForm.bind(this));
     },
-    submitForm: function(e){
-            e.preventDefault();
-            this.addUser();
-            
-    },
-    addUser: function(){
-        this.users.push({
-                username:username.value,
-                role:role.value,
-                bio:bio.value,
-                photo:photo.value,
-            });
-            form.reset();
+    submitForm: function (e) {
+        e.preventDefault();
+        this.addUser();
 
     },
-    removeUser: function() {},
- };
- userManager.init();
- console.log(userManager.users);
+    addUser: function () {
+        this.users.push({
+            username: username.value,
+            role: role.value,
+            bio: bio.value,
+            photo: photo.value,
+        });
+        form.reset();
+        this.renderUi();
+
+    },
+    renderUi: function () {
+        this.users.forEach(function (user) {
+
+            const card = document.createElement("div");
+            card.className =
+                "bg-white/10 backdrop-blur-lg rounded-xl p-6 text-center shadow-lg";
+
+
+            const img = document.createElement("img");
+            img.src = user.photo;
+            img.className =
+                "w-20 h-20 rounded-full mx-auto mb-4 border border-gray-500";
+
+
+            const name = document.createElement("h3");
+            name.className = "text-lg font-semibold";
+            name.textContent = user.username;
+
+
+            const role = document.createElement("p");
+            role.className = "text-sm text-gray-300";
+            role.textContent = user.role;
+
+
+            const bio = document.createElement("p");
+            bio.className = "text-xs text-gray-400 mt-2";
+            bio.textContent = user.bio;
+
+
+            card.append(img, name, role, bio);
+            document.querySelector(".users").innerHTML ="";
+            document.querySelector(".users").appendChild(card);
+        }
+        );
+    },
+    removeUser: function () { },
+};
+userManager.init();
+console.log(userManager.users);
