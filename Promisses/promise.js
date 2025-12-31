620e82fd6225847597dfcb0d79014d9e897eeaf0
Chinmayi-H-M <hmschinmayi06@gmail.com>
2025-12-31 17:58:06 +0530

    Promises

---
+++ Promisses/promise.js
@@ -0,0 +1,13 @@
+let pr = new Promise(function(res,rej){
+    setTimeout(() => {
+        let rn = Math.floor(Math.random()*10);
+        if(rn>5) res(rn);
+        else rej(rn);
+    }, 3000);
+})
+
+pr.then(function (val){
+    console.log(val);
+}).catch(function(val){
+    console.log(val);
+})
