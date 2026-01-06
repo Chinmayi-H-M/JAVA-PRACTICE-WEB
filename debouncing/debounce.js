1ee4e6d4f1ef6e11333abb6f800f1e27952af7b3
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-06 17:41:21 +0530

    Debouncing

--- debouncing/debounce.js
+++ debouncing/debounce.js
@@ -1,6 +1,12 @@
-function debounce(){
+function debounce(fnc,delay){
+    let timer;
+    return function(...args){
+        clearTimeout(timer);
+        timer = setTimeout(function(){
+            fnc(...args);
+        },delay)   }
 
 }
-document.querySelector("input").addEventListener("input", function(){
+document.querySelector("input").addEventListener("input",debounce(function(){
     console.log("hey");
-})
+},1000));
