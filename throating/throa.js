2af326689b4c1be1e5a4ed9ad89b5c9244fd03d1
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-07 06:20:19 +0530

    Throttle

--- throating/throa.js
+++ throating/throa.js
@@ -1,3 +1,21 @@
 //debounce = we will give a dela, when that delay happens we will get reaction for action
 
-//throttle->interval par chalunga, action agar hota raha and aapne ek interval bataya tio utne interval me aapka event chalega
+//throttle->interval par chalunga, action agar hota raha and aapne ek interval bataya tio utne interval me aapka event
+// 
+
+function throttle(fnc,delay){
+    let timer =0;
+    return function(...args){
+        let now = Date.now();
+        if(now-timer>=delay){
+            time = now;
+            fnc(...args);
+        }
+    }
+    
+}
+
+document.querySelector("input").addEventListener("input",throttle(function(){
+    console.log(ran);
+},1000
+))
