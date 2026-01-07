0a2de8e745a19db57ef413c4ac61134c7a1c52a6
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-07 06:13:43 +0530

    Throttle

--- throating/throa.js
+++ throating/throa.js
@@ -1,1 +1,3 @@
 //debounce = we will give a dela, when that delay happens we will get reaction for action
+
+//throttle->interval par chalunga, action agar hota raha and aapne ek interval bataya tio utne interval me aapka event chalega
