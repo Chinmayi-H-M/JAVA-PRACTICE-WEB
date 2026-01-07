258cb2a7c08759e2e85753fe54a67e0771e5a9d9
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-07 07:06:55 +0530

    Lazyloading

--- lazyloading/lazy.js
+++ lazyloading/lazy.js
@@ -1,11 +1,22 @@
 let img= document.querySelectorAll("img");
-const observer = new IntersectionObserver{
+const observer = new IntersectionObserver(
     function(entries, observer){
-        entries.forEach(function(entry)){
-            
+        entries.forEach(function(entry){
+            if(entry.isIntersecting){
+                const img = entry.target;
+                img.src= img.dataset.src;
+                img.classList.add("loaded");
+                observer.unobserve(entry);
+            }
         });
-    }
+    },
     {
-        
+       root:null,
+       threshold: 0.1,
+
     }
-}
+)
+
+img.forEach(function (img){
+    observer.observe(img);
+})
