5b3d2a2cb0babaf80036b642e7e41048d8405771
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-07 07:02:06 +0530

    Lazyloading

--- lazyloading/lazy.js
+++ lazyloading/lazy.js
@@ -1,1 +1,11 @@
 let img= document.querySelectorAll("img");
+const observer = new IntersectionObserver{
+    function(entries, observer){
+        entries.forEach(function(entry)){
+            
+        });
+    }
+    {
+        
+    }
+}
