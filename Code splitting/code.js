ee3a36b963ef61aa8eb31eb68d946528d9b563ee
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-09 07:35:35 +0530

    Codesplitting

--- Code splitting/code.js
+++ Code splitting/code.js
@@ -1,3 +1,6 @@
-function veryHeavy(){
-    
-}
+const btn = document.querySelector("button");
+
+btn.addEventListener("click",async function(){
+    let heavy = await import("./heavy.js");
+    heavy.veryHeavy();
+})
---
+++ Code splitting/heavy.js
@@ -0,0 +1,4 @@
+export function veryHeavy(){
+    console.log("Giant feature loaded...");
+    alert("Heavy feature loading completed");
+}
