57a8e8b5fa0e3199803e266814132e70b9e727ba
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-09 07:40:41 +0530

    Codesplitting

--- Code splitting/code.js
+++ Code splitting/code.js
@@ -1,6 +1,16 @@
+import { createElement } from "react";
+
 const btn = document.querySelector("button");
 
 btn.addEventListener("click",async function(){
     let heavy = await import("./heavy.js");
     heavy.veryHeavy();
 })
+
+//In this the dom will reload and it causes the website to lag
+const ul= document.querySelector(ul);
+for (let i=0;i<100;i++){
+   const li= createElement("li");
+   li.textContent = i;
+   ul.appendChild();
+}
