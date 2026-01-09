79aa581defa13ea828fe0afee0c93ce57e4acf25
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-01-09 07:51:12 +0530

    Codesplitting

--- Code splitting/code.js
+++ Code splitting/code.js
@@ -1,4 +1,3 @@
-import { createElement } from "react";
 
 const btn = document.querySelector("button");
 
@@ -9,17 +8,30 @@ btn.addEventListener("click",async function(){
 
 //In this the dom will reload and it causes the website to lag
 //const ul= document.querySelector(ul);
-for (let i=0;i<100;i++){
+/*for (let i=0;i<100;i++){
    const li= document.createElement("li");
    li.textContent = i;
    ul.appendChild(li);
 }
 
 const ul= document.querySelector(ul);
-const space = document.createDocumentFragment();
+const space = document.createDocumentFragment();// this will resolve the issue
 for (let i=0;i<100;i++){
    const li= document.createElement("li");
    li.textContent = i;
    space.appendChild(li);
 }
-ul.appendChild(space);
+ul.appendChild(space);*/
+
+//Time leeking
+let count =0;
+const int = setInterval(() => {
+    if(count<10){
+        count ++;
+        console.log(count);
+    }
+    else{
+        clearInterval(int);//If we didnt write the clearInterval then it will keep on running but it doesn't print anything because already if consditon became false
+        console.log("Finished");
+    }
+}, 500);
